0e2e55bbc79ce6fb3ff8c7b511b92bb6f11be401
bhimdahal <bhimdahal2446@gmail.com>
2018-08-22 14:17:48 -0400

    adding files

---
+++ temp/app/app.module.js
@@ -0,0 +1,9 @@
+angular.module('fittanyUiApp', [
+            'ngStorage',
+            'ngResource',
+            'ngAria',
+            'ui.bootstrap',
+            'ui.bootstrap.datetimepicker',
+            'ui.router',
+            'angular-loading-bar'
+        ]);
